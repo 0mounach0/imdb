b82913a54fd5f33e7dfed2e5be4fe21d180db4bd
Mouad MOUNACH <me.mounach@gmail.com>
2023-07-25 12:09:27 +0200

    refact: init atomic design folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { MovieCardComponent } from './components/movie-card/movie-card.component';
+import { NavbarComponent } from './components/organisms/navbar/navbar.component';
+import { MovieCardComponent } from './components/organisms/movie-card/movie-card.component';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { httpTranslateLoader } from './config/translate-loader.config';
-import { PaginationComponent } from './components/pagination/pagination.component';
+import { PaginationComponent } from './components/organisms/pagination/pagination.component';
 import { FormsModule } from '@angular/forms';
-import { EditMovieModalComponent } from './components/edit-movie-modal/edit-movie-modal.component';
-import { ScrollToTopComponent } from './components/scroll-to-top/scroll-to-top.component';
-import { MovieInfoComponent } from './components/movie-info/movie-info.component';
+import { EditMovieModalComponent } from './components/molecules/movie-info/edit-movie-modal/edit-movie-modal.component';
+import { ScrollToTopComponent } from './components/organisms/scroll-to-top/scroll-to-top.component';
+import { MovieInfoComponent } from './components/molecules/movie-info/movie-info.component';
 import { DebouceKeyupDirective } from './directives/debouce-keyup.directive';
 
 @NgModule({
